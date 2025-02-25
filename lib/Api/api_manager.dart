ea7eb5869c2dda59dfb2477bc7ef505094e69aba
Dell <mariammohamedahmedahmed80@gmail.com>
2025-02-26 01:10:19 +0200

    * finish timer

--- lib/Api/api_manager.dart
+++ lib/Api/api_manager.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
 
-import 'package:project_islami/models/radio_response_model.dart';
 import 'package:http/http.dart' as http;
+import 'package:intl/intl.dart';
+import 'package:project_islami/models/azkar_model.dart';
+import 'package:project_islami/models/radio_response_model.dart';
 import 'package:project_islami/models/reciters_response_model.dart';
 
 class ApiManager {
@@ -24,6 +26,20 @@ class ApiManager {
       var jsonResponse = jsonDecode(response.body);
 
       return RecitersResponseModel.fromJson(jsonResponse);
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  static Future<AzkarModel> getPrayingData() async {
+    try {
+      String date = DateFormat("dd-MM-yyy").format(DateTime.now());
+
+      Uri uri = Uri.parse(
+          "https://api.aladhan.com/v1/timingsByCity/$date?city=cairo&country=egypt&method=8");
+      var response = await http.get(uri);
+      var jsonResponse = jsonDecode(response.body);
+      return AzkarModel.fromJson(jsonResponse);
     } catch (e) {
       throw e;
     }
--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -6,4 +6,6 @@ class AppColors {
   static Color whiteColor = Color(0xffffffff);
   static Color blackColor = Color(0x99202020);
   static Color titleTextColor = Color(0xffFEFFE8);
+  static Color brownColor = Color(0xff856B3F);
+  static Color gradientColor = Color(0xffB19768);
 }
